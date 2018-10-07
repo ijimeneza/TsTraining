82b9d8bafe6b84c8fd5dba71df5fe3ddade8e177
Ignacio Jimenez <ignacio.jimenez.alfaro@gmail.com>
2018-10-07 19:48:57 +0200

    initial

---
+++ src/scripts/main.ts
@@ -0,0 +1,21 @@
+interface IPoint {
+    getDist(): number;
+}
+
+module Shapes {
+    export class Point implements IPoint {
+        
+        constructor (public x: number, public y: number) {}
+
+        getDist(): number {
+            return Math.sqrt(this.x * this.x + this.y*this.y);
+        }
+
+        static origin = new Point (0,0);
+        
+    }
+}
+
+var p: IPoint = new Shapes.Point(3, 4);
+
+var dist = p.getDist();
