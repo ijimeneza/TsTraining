dbfae8f733a98cc78333010dfa1cd7c4a9f3907d
Ignacio Jimenez <ignacio.jimenez.alfaro@gmail.com>
2018-10-16 22:55:37 +0200

    modules and express

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,35 +1,7 @@
-// interface IPoint {
-//     getDist(): number;
-// }
-
-// module Shapes {
-//     export class Point implements IPoint {
-        
-//         constructor (public x: number, public y: number) {}
-
-//         getDist(): number {
-//             return Math.sqrt(this.x * this.x + this.y*this.y);
-//         }
-
-//         static origin = new Point (0,0);
-        
-//     }
-// }
-
-// var p: IPoint = new Shapes.Point(3, 4);
-
-// var dist = p.getDist();
-
-// console.log('Point: ' + dist);
-
 // window.onload = function () {
-//     alert('The point:' + 'kk');//dist);
+//     alert('lets go');
+//     var folder = new Folder('the Folder');
+//     folder.setDocuments(new EmployeeDocument('a doc'), new EmployeeDocument('another doc'));
+//     folder.some();
 // }
-
-
-window.onload = function () {
-    alert('lets go');
-    var folder = new Folder('the Folder');
-    folder.setDocuments(new EmployeeDocument('a doc'), new EmployeeDocument('another doc'));
-    folder.some();
-}
+toastr.info("whatever");
--- src/scripts/server.ts
+++ src/scripts/server.ts
@@ -1,12 +1,19 @@
 
 
-import http = require('http');
+// import http = require('http');
+// var fs = require('fs');
+// var defaultPage = fs.readFileSync('..\default.html');
+// var server = http.createServer(function(req, res)
+// {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end(defaultPage);
+// });
 
-var server = http.createServer(function(req, res)
-{
-    res.writeHead(200);
-    res.end('Hello from Node');
+// server.listen(1500);
+// console.log('Listening...');
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic('../')).listen(8080, function(){
+    console.log('Server running on 8080...');
 });
-
-server.listen(1500);
-console.log('Listening...');
